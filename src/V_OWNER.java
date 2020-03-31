018b5fa4204473e4d526caabd7db9b5d35330f34
leamladenic <lea.mladenic3@gmail.com>
2020-03-31 17:36:44 +0200

    start of owner window

---
+++ src/V_OWNER.java
@@ -0,0 +1,16 @@
+
+
+/**
+ *
+ * @author Lea
+ */
+public class V_OWNER {
+    
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private String email;
+    private String address;
+    
+}
